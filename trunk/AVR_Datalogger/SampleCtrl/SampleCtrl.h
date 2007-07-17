82f1fd4a6442ec476f4b74de592589542eb7a004
adrian.gin <adrian.gin@b3a837a6-dc34-0410-8231-87d81a9a7aab>
2007-07-17 10:49:50 +0000

    Fixed UART Problem. Added Timer Functions.

--- trunk/AVR_Datalogger/SampleCtrl/SampleCtrl.h
+++ trunk/AVR_Datalogger/SampleCtrl/SampleCtrl.h
@@ -1,12 +1,14 @@
 
-#define SC_COMPARE_RATE 125
+#define SC_COMPARE_RATE 250
+
+#define SC_PRESCALER 32
 
 /* Period of compare in us */
-#define SC_COMPARE_PERIOD  ((SC_COMPARE_RATE) / ((F_CPU/8) / 1e6))
+#define SC_COMPARE_PERIOD  ((SC_COMPARE_RATE) / ((F_CPU/SC_PRESCALER) / 1e6))
 
 /* Number of counts to reach 1 milli-second */
 #define SC_MILLISECOND     (1000 / SC_COMPARE_PERIOD)
-
+#define SC_SECONDS 10
 
 /* Timer Start / Stop commands */
 #define SC_EnableTimer()   TIMSK |= (1 << OCIE2)
@@ -15,3 +17,9 @@
 /* Sets up the required timers etc */
 void SC_Init(void);
 
+typedef struct _softTimer
+{
+	uint32_t timeOut;
+	(void*) 	timerFunction(void);
+	
+} SoftTimer;
