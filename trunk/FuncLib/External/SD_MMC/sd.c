82d51c63941ce819bda1ae65a9b3a63d3fac9c09
adrian.gin <adrian.gin@b3a837a6-dc34-0410-8231-87d81a9a7aab>
2007-07-30 12:09:21 +0000

    Integrated DISK IO functions with the SD card library.
    *UNSTABLE* !! Need to do some compiling testing.

--- trunk/FuncLib/External/SD_MMC/sd.c
+++ trunk/FuncLib/External/SD_MMC/sd.c
@@ -6,6 +6,12 @@
 #include <util/delay.h>
 #include "mmculib/uint8toa.h"
 #include "hardUart/hardUart.h"
+#include "diskio/diskio.h"
+
+#define SD_ERROR 		0
+#define SD_SUCCESS 	1
+
+DSTATUS MMC_Stat = STA_NOINIT;
 
 /* returns 0 if success */
 uint8_t SD_Init(void)
@@ -40,7 +46,7 @@ uint8_t SD_Init(void)
             
       if( i > MMC_MAX_RETRIES )
       {
-         return 1;
+         return SD_ERROR;
       }
    }
    
@@ -54,7 +60,7 @@ uint8_t SD_Init(void)
       }    
       if( i > 200 )
       {
-         return 1;
+         return SD_ERROR;
       }
    }
    
@@ -65,7 +71,8 @@ uint8_t SD_Init(void)
    MMC_CS_PORT |= (1 << MMC_CS_PIN);
    SPCR |= (1 << CPHA) | (1 << SPR1);
    
-   return 0;
+   MMC_Stat &= ~(STA_NOINIT); /// Set to initialised
+   return SD_SUCCESS;
     
 
 
@@ -94,19 +101,9 @@ void SD_Shutdown(void)
    SPCR |= ((1 << CPHA) | (1<<SPE));     
 }
 
-uint8_t SD_SendCommand(uint8_t cmd, uint32_t arg)
-{
-   uint8_t r1;
-   MMC_CS_PORT &= ~(1 << MMC_CS_PIN);
-   r1 = SD_Command(cmd, arg);   
-   MMC_CS_PORT |= ~(1 << MMC_CS_PIN);
-   
-   return r1;
-      
-}
+
 
 /* Note that Clock Phase should be set to zero */ 
-
 uint8_t SD_Command(uint8_t cmd, uint32_t arg)
 {
 	uint8_t r1;
@@ -146,57 +143,108 @@ uint8_t SD_Command(uint8_t cmd, uint32_t arg)
 }
 
 
-
-
-
-
-
-
-uint8_t SD_Read(uint32_t sector, uint8_t* buffer)
+/* Subroutine which reads 'byteCount' bytes from the SPI and writes it to buffer */
+uint8_t SD_ReadBlock(uint8_t* buffer, uint16_t byteCount)
 {
-	uint8_t r1;
+	uint16_t i;
+	uint8_t retry = 0;	
+	/** Wait for start block */
+	while( SPI_RxByte() != MMC_STARTBLOCK_READ)
+	{
+		if(retry++ > MMC_MAX_RETRIES)
+		{
+			return SD_ERROR;		
+		}
+	}
+	/* read in data */
+	for(i=0; i < byteCount; i++)
+	{
+		*buffer++ = SPI_RxByte();
+	}
+	SPI_RxByte(); /// read 16-bit CRC
+	SPI_RxByte()
+	return SD_SUCCESS;	
+}
+
+/* Used by SD_DISKREAD */
+/** Writes the buffer to the sector. secCount is the number of sectors to write */	
+uint8_t SD_Read(uint8_t* buffer, uint32_t sector, uint8_t secCount )
+{
 	uint16_t i;
    uint8_t outputString[5];
 
 	// assert chip select
    MMC_CS_PORT &= ~(1 << MMC_CS_PIN);
-	// issue command
-	r1 = SD_Command(MMC_READ_SINGLE_BLOCK, sector<<9);
-
-   uartNewLine();
-   uartTxString("SD Read R1 Response: ");
-   uint8toa(r1, outputString);
-   uartTxString(outputString);   
-   uartNewLine();   
-
-	// check for valid response
-	if(r1 != 0x00)
-		return r1;
-	// wait for block start
-	while(SPI_TxByte(0xFF) != MMC_STARTBLOCK_READ);
-	// read in data
-	for(i=0; i<0x200; i++)
-	{
-		*buffer++ = SPI_TxByte(0xFF);
+   
+   
+	if (secCount == 1) {									// Single block read 
+		if ((SD_Command(MMC_READ_SINGLE_BLOCK, sector) == 0)		// READ_SINGLE_BLOCK 
+			&& SD_ReadSector(buffer, 512))
+			secCount = 0;
 	}
-	// read 16-bit CRC
-	SPI_TxByte(0xFF);
-	SPI_TxByte(0xFF);
-
+	else {	// Multiple block read
+		if (SD_Command(MMC_READ_MULTIPLE_BLOCK, sector) == 0) 
+		{	// READ_MULTIPLE_BLOCK 
+			do 
+			{
+				if (!SD_ReadSector(buffer, 512)) break;
+				buffer += 512;
+			} while (--secCount);
+			MMC_send_cmd(MMC_STOP_READ_TRANS, 0);				// STOP_TRANSMISSION 
+		}
+	}
+  
 	// wait until card not busy
-	while(!SPI_RxByte());
+	while(!SPI_RxByte());	
 
-		
    /* Release and return clock phase and speed back to default */
    MMC_CS_PORT |= (1 << MMC_CS_PIN);
    SPCR |= (1 << CPHA) | (1 << SPR1);
   
-   
 	// return success
-	return 0;
+	return secCount ? RES_ERROR : RES_OK;
 }
 
-uint8_t SD_Write(uint32_t sector, uint8_t* buffer)
+
+
+
+/** Subroutine which writes 512 bytes of buffer to the SD Card*/
+/* Token is the data token to send */
+uint8_t SD_WriteSector(uint8_t* buffer, uint8_t token)
+{
+	uint8_t retry = 0;	
+
+	/** Wait until card is ready */
+	while( SPI_RxByte() != 0xFF)
+	{
+		if(retry++ > MMC_MAX_RETRIES)
+		{
+			return SD_ERROR;		
+		}
+	}	
+
+	SPI_TxByte(token);
+
+	/* Write data */
+	for(i=0; i<0x200; i++)
+	{
+		SPI_TxByte(*buffer++);
+	}
+	SPI_RxByte(); /// read 16-bit CRC
+	SPI_RxByte()
+	
+	// read data response token
+	if( (SPI_RxByte() & MMC_DR_MASK) != MMC_DR_ACCEPT)
+		return SD_ERROR;	
+	
+	
+	return SD_SUCCESS;	
+}
+
+
+
+/* Writes 'secCount' sectors of buffer to the address sector */
+uint8_t SD_Write(uint8_t* buffer, uint32_t sector, uint8_t secCount)
 {
 	uint8_t r1;
 	uint16_t i;
@@ -204,47 +252,123 @@ uint8_t SD_Write(uint32_t sector, uint8_t* buffer)
    
 	// assert chip select
    MMC_CS_PORT &= ~(1 << MMC_CS_PIN);
-	// issue command
-	r1 = SD_Command(MMC_WRITE_BLOCK, sector<<9);
    
    
-
-	// check for valid response
-	if(r1 != 0x00)
-	{
-      uartNewLine();
-      uartTxString("SD Write R1 Response: ");
-      uint8toa(r1, outputString);
-      uartTxString(outputString);   
-      uartNewLine();        
-      
-		return r1;
-   }
-	// send dummy
-	SPI_TxByte(0xFF);
-	// send data start token
-	SPI_TxByte(MMC_STARTBLOCK_WRITE);
-	// write data
-	for(i=0; i<0x200; i++)
-	{
-		SPI_TxByte(*buffer++);
+	if (secCount == 1) {									// Single block write 
+		if ((SD_Command(MMC_WRITE_SINGLE_BLOCK, sector) == 0)
+			&& SD_WriteSector(buffer, MMC_STARTBLOCK_WRITE))
+			secCount = 0;
 	}
-	// write 16-bit CRC (dummy values)
-	SPI_TxByte(0xFF);
-	SPI_TxByte(0xFF);
-	// read data response token
-	r1 = SPI_RxByte();
-	if( (r1&MMC_DR_MASK) != MMC_DR_ACCEPT)
-		return r1;
-
+	else {	// Multiple block Write
+		if (SD_Command(MMC_WRITE_MULTIPLE_BLOCK, sector) == 0) 
+		{ 
+			do 
+			{
+				if (!SD_WriteSector(buffer, MMC_STARTBLOCK_MWRITE)) break;
+				buffer += 512;
+			} while (--secCount);
+			SPI_TxByte(MMC_STOPTRAN_WRITE);				// STOP_TRANSMISSION 
+		}
+	}
+  
 	// wait until card not busy
-	while(!SPI_RxByte());
+	while(!SPI_RxByte());	
 
    /* Release and return clock phase and speed back to default */
    MMC_CS_PORT |= (1 << MMC_CS_PIN);
    SPCR |= (1 << CPHA) | (1 << SPR1);
-
-	
+  
 	// return success
+	return secCount ? RES_ERROR : RES_OK;
+}
+
+
+
+
+
+/** Disk IO Functions */
+DSTATUS SD_disk_status (void)
+{
 	return 0;
+}
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+/** Ctrl is the control command to send */
+/** buff is the memory to read or write the control data to */
+DRESULT disk_ioctl (	uint8_t ctrl,	void *buff )
+{
+	DRESULT res;
+	uint8_t n, csd[16], *ptr = buff;
+	uint16_t csize;
+
+	res = RES_ERROR;
+
+
+	if (Stat & STA_NOINIT) return RES_NOTRDY;
+
+   /* Select the card */
+   MMC_CS_PORT &= ~(1 << MMC_CS_PIN);
+
+	switch (ctrl) 
+	{
+		case GET_SECTOR_COUNT :	/* Get number of sectors on the disk (DWORD) */
+			if ((SD_Command(MMC_SEND_CSD, 0) == 0) && SD_ReadSector(csd, 16)) {
+				if ((csd[0] >> 6) == 1) {	/* SDC ver 2.00 */
+					csize = csd[9] + ((uint16_t)csd[8] << 8) + 1;
+					*(uint32_t*)buff = (uint32_t)csize << 10;
+				} else {					/* MMC or SDC ver 1.XX */
+					n = (csd[5] & 15) + ((csd[10] & 128) >> 7) + ((csd[9] & 3) << 1) + 2;
+					csize = (csd[8] >> 6) + ((uint16_t)csd[7] << 2) + ((uint16_t)(csd[6] & 3) << 10) + 1;
+					*(uint32_t*)buff = (uint16_t)csize << (n - 9);
+				}
+				res = RES_OK;
+			}
+			break;
+	
+		case GET_SECTOR_SIZE :	/* Get sectors on the disk (WORD) */
+			*(uint16_t*)buff = 512;
+			res = RES_OK;
+			break;
+	
+		case CTRL_SYNC :	/* Make sure that data has been written */
+				while(!SPI_RxByte());
+				res = RES_OK;
+			break;
+	
+		case MMC_GET_CSD :	/* Receive CSD as a data block (16 uint8_ts) */
+			if (SD_Command(MMC_SEND_CSD, 0) == 0		/* READ_CSD */
+				&& SD_ReadSector(ptr, 16))
+				res = RES_OK;
+			break;
+	
+		case MMC_GET_CID :	/* Receive CID as a data block (16 uint8_ts) */
+			if (SD_Command(MMC_SEND_CID, 0) == 0		/* READ_CID */
+				&& SD_ReadSector(ptr, 16))
+				res = RES_OK;
+			break;
+	
+		case MMC_GET_OCR :	/* Receive OCR as an R3 resp (4 uint8_ts) */
+			if (SD_Command(MMC_READ_OCR, 0) == 0) {	/* READ_OCR */
+				for (n = 0; n < 4; n++)
+					*ptr++ = SPI_RxByte();
+				res = RES_OK;
+			}
+	
+		case MMC_GET_TYPE :	/* Get card type flags (1 uint8_t) */
+			*ptr = CardType;
+			res = RES_OK;
+			break;
+	
+		default:
+			res = RES_PARERR;
+	}
+
+   /* Deselect the card */
+   MMC_CS_PORT |= (1 << MMC_CS_PIN);
+	SPI_RxByte();			/* Idle (Release DO) */
+
+
+	return res;
 }
